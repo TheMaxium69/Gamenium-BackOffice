d28cbfa51a033208e7d4a5301bc935cacde2a3ea
bthiebau <bastien.thiebaut@hotmail.com>
2025-01-30 15:13:54 +0100

    update
    Co-Authored-By: Adele <166720543+Adeleloff@users.noreply.github.com>
    Co-Authored-By: Oscar <159458439+05c4rR@users.noreply.github.com>
    Co-Authored-By: TheMaximeSan <maxime.tournier@tyrolium.fr>

--- src/app/moderation/warn/warn.component.ts
+++ src/app/moderation/warn/warn.component.ts
@@ -1,24 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { WarnInterface } from 'src/app/-interface/warn.interface';
 import { WarnService } from 'src/app/-service/warn.service';
 import { AppComponent } from 'src/app/app.component';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {MatPaginator, MatPaginatorModule} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-warn',
   templateUrl: './warn.component.html',
-  styleUrls: ['./warn.component.css']
+  styleUrls: ['./warn.component.css'],
+  standalone: true,
+  imports: [MatTableModule, MatPaginatorModule]
 })
-export class WarnComponent implements OnInit{
-
+export class WarnComponent implements OnInit, AfterViewInit{
+  displayedColumns: string[] = ['ID', 'Signalé par', 'Motif', 'Objet', 'Date du signalement', 'Description'];
   allWarns: WarnInterface[] = [];
+  dataSource = new MatTableDataSource<WarnInterface>(this.allWarns);
+ 
  constructor(
   private warnService: WarnService,
   private app:AppComponent
  ){}
 
+
  ngOnInit(): void {
    this.getAllWarn()
  }
+
+ @ViewChild(MatPaginator) paginator: MatPaginator;
+
+ ngAfterViewInit(): void {
+  this.dataSource.paginator = this.paginator;
+ }
+ 
 
  getAllWarn(){
   this.warnService.getAllWarn(this.app.setURL(), this.app.createCorsToken()).subscribe((response: {
