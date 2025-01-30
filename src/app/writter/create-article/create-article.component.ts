3f9e23276cd385810f8c388498fb63bddf5f92bd
Erynn <vandreerynn@gmail.com>
2025-01-30 14:46:08 +0100

    Form add actu

--- src/app/writter/create-article/create-article.component.ts
+++ src/app/writter/create-article/create-article.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActualityService } from 'src/app/-service/actuality.service';
+import { AppComponent } from 'src/app/app.component';
 
 @Component({
   selector: 'app-create-article',
@@ -7,4 +9,33 @@ import { Component } from '@angular/core';
 })
 export class CreateArticleComponent {
 
+title: string = "";
+content: string = "";
+picture_id: number = 0;
+game_id: number = 0;
+
+
+  constructor(
+    protected app:AppComponent,
+    private actualityService:ActualityService
+  ) {
+
+  }
+
+  submitArticle(){
+
+    const url = this.app.setURL();
+    const option = this.app.createCorsToken();
+    const body = {
+      title:this.title,
+      content:this.content,
+      picture_id:this.picture_id,
+      game_id:this.game_id,
+      createdAt:new Date(),
+    }
+
+    this.actualityService.createPostActu(body, url, option).subscribe(response => {
+      console.log("Article créer", response)
+    } )
+  }
 }
