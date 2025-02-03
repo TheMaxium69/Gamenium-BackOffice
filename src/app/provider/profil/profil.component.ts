953cb14898491c6a29e90668603dd7c53c2514b0
Erynn Vandre <vandreerynn@gmail.com>
2025-02-03 14:54:47 +0100

    WIP Edit provider

--- src/app/provider/profil/profil.component.ts
+++ src/app/provider/profil/profil.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, numberAttribute, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, ElementRef, Input, numberAttribute, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
 import { ApicallInterface } from 'src/app/-interface/apicall.interface';
 import { ProviderInterface } from 'src/app/-interface/provider.interface';
 import { ProviderService } from 'src/app/-service/provider.service';
+import { UploadService } from 'src/app/-service/upload.service';
 import { AppComponent } from 'src/app/app.component';
 
 @Component({
@@ -11,14 +12,36 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class ProfilComponent {
 
+  @Input({transform: numberAttribute})
+  public id: number|null = null;
+  public providerSelected: ProviderInterface | undefined;
+
   constructor(
     protected app: AppComponent,
-    private providerService: ProviderService
+    private providerService: ProviderService,
+    private uploadService: UploadService,
   ) {}
 
-  providerSelected: ProviderInterface | undefined;
+  @ViewChild('fileInput') fileInput!: ElementRef; 
 
-  updateProfil() {
+  /* Ouvrir le sélecteur de fichier */
+  triggerFileSelect() {
+    this.fileInput.nativeElement.click();
+  }
+
+  /* Gérer la sélection de l'image */
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const url = this.app.setURL();
+      const option = this.app.createCorsToken();
+      this.uploadService.uploadPostActuPhoto(file, url, option).subscribe(response => {
+        this.providerSelected!.picture = response.result; // Mise à jour de l'image
+      });
+    }
+  }
+
+  updateProfil() {  
     if (!this.providerSelected) {
       console.error("Aucun provider sélectionné");
       return;
@@ -28,14 +51,15 @@ export class ProfilComponent {
     const option = this.app.createCorsToken();
 
     const body = {
-      title: this.providerSelected.displayName,
+      displayName: this.providerSelected.displayName,
       content: this.providerSelected.content,
       picture_id: this.providerSelected.picture?.id,
+      founded_at: this.providerSelected.founded_at,
     };
-    
+
 
     this.providerService.updateProvider(this.providerSelected.id, body, url, option).subscribe(response => {
-      console.log("Article mis à jour", response);
+      console.log("Éditeur mis à jour", response);
     });
   }
 }
