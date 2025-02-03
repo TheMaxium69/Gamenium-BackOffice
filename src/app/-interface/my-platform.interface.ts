8c6fc7ebd0870074731d1748ad81269bf5a69b5e
bthiebau <bthiebaut.dev@gmail.com>
2025-02-03 12:01:57 +0100

    update

--- src/app/-interface/my-platform.interface.ts
+++ src/app/-interface/my-platform.interface.ts
@@ -1,8 +1,10 @@
 import {PlateformInterface} from "./plateform.interface";
+import { UserInterface } from "./user.interface";
 
 export interface myPlatformInterface {
 
   id:number|undefined,
+  user:UserInterface,
   plateform:PlateformInterface,
   added_at:Date
 
