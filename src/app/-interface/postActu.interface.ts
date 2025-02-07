4cb8efc1f4f2b77ddc4f6b25e51591604e170a9b
TheMaxium69 <mxm.tournier@gmail.com>
2025-02-07 11:52:42 +0100

    final actu modo

--- src/app/-interface/postActu.interface.ts
+++ src/app/-interface/postActu.interface.ts
@@ -1,5 +1,6 @@
 import {UserInterface} from "./user.interface";
 import {PictureInterface} from "./picture.interface";
+import {ProviderInterface} from "./provider.interface";
 
 export interface PostActuInterface {
     id: number,
@@ -11,4 +12,5 @@ export interface PostActuInterface {
     user: UserInterface,
     game_id: number,
     created_at: Date,
+    Provider:ProviderInterface|undefined,
 }
