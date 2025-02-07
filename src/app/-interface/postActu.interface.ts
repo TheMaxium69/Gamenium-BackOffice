11ccfa75fa45e6e9cf0b0e284104c3b4b6f5d93d
TheMaxium69 <mxm.tournier@gmail.com>
2025-02-07 10:55:37 +0100

    update

--- src/app/-interface/postActu.interface.ts
+++ src/app/-interface/postActu.interface.ts
@@ -1,9 +1,14 @@
+import {UserInterface} from "./user.interface";
+import {PictureInterface} from "./picture.interface";
+
 export interface PostActuInterface {
     id: number,
     title:string,
     content: string,
     picture_id: number,
+    picture: PictureInterface
     user_id: number,
+    user: UserInterface,
     game_id: number,
     created_at: Date,
 }
