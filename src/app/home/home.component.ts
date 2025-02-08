4ea67102f0d62e8eeb073edc6bbab76cbd0ed64d
TheMaxium69 <mxm.tournier@gmail.com>
2025-02-08 19:50:11 +0100

    random plateform

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppComponent} from "../app.component";
+import {PlatformService} from "../-service/platform.service";
+import {PlateformInterface} from "../-interface/plateform.interface";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit{
 
-  constructor(protected app: AppComponent) {
+  constructor(protected app: AppComponent,
+              private platformSerivce:PlatformService) {
+  }
+
+  plateformRandom: PlateformInterface | undefined;
+
+  ngOnInit() {
+
+
+    this.randomPlateform()
+
+  }
+
+
+  randomPlateform(){
+    this.platformSerivce.getPlateformsRandom(this.app.setURL(), this.app.createCorsToken()).subscribe((response : { message: string, result: PlateformInterface }) => {
+
+      if (response.message === "good") {
+        this.plateformRandom = response.result;
+      }
+
+    })
   }
 
 }
